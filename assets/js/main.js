b8ea018bfc9a6ca14da40dc40f0f56e0a81fbca9
nguyencongthang2509 <congthang25092003@gmail.com>
2023-02-28 21:00:14 +0700

    no message

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,20 @@ const page_link = document.querySelectorAll(".page-link");
 const toggle = document.querySelector(".toggle");
 const p_tag = document.querySelectorAll("p.copyright.text-muted");
 const p1_tag = document.querySelectorAll("p.theme-by.text-muted");
-const allElements = document.querySelectorAll('*');
-const filteredElements = Array.from(allElements).filter(element => !element.classList.contains('container-md') && !element.classList.contains('row') && !element.classList.contains('col-xl-8') && !element.classList.contains('avatar-container') && element.tagName !== 'H1' && !element.classList.contains('post-heading') && !element.classList.contains('post-subheading') && !element.classList.contains('fa-sun') && !element.classList.contains('fa-moon') && !element.classList.contains('post-meta'));
+const allElements = document.querySelectorAll("*");
+const filteredElements = Array.from(allElements).filter(
+  (element) =>
+    !element.classList.contains("container-md") &&
+    !element.classList.contains("row") &&
+    !element.classList.contains("col-xl-8") &&
+    !element.classList.contains("avatar-container") &&
+    element.tagName !== "H1" &&
+    !element.classList.contains("post-heading") &&
+    !element.classList.contains("post-subheading") &&
+    !element.classList.contains("fa-sun") &&
+    !element.classList.contains("fa-moon") &&
+    !element.classList.contains("post-meta")
+);
 
 var index = 0;
 toggle.addEventListener("click", () => {
@@ -12,14 +24,14 @@ toggle.addEventListener("click", () => {
     for (var i = 0; i < p_tag.length; i++) {
       p_tag[i].style.color = "white !important";
       p1_tag[i].style.color = "white !important";
-      const moon = document.querySelector("far.fa-mooon")
-      moon.classList.toggle("dark-skin");
     }
   } else {
     toggle.firstElementChild.className = "fa fa-sun";
     for (var i = 0; i < p_tag.length; i++) {
       p_tag[i].style.color = "gray !important";
       p1_tag[i].style.color = "gray !important";
+      const moon = document.querySelector("far.fa-mooon");
+      moon.classList.toggle("dark-skin");
     }
   }
   for (var i = 0; i < filteredElements.length; i++) {
