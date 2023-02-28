9ba4b320603fcbece49aa2e8bc9516926e56cc7c
nguyencongthang2509 <congthang25092003@gmail.com>
2023-02-28 20:46:38 +0700

    no message

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,8 @@ const page_link = document.querySelectorAll(".page-link");
 const toggle = document.querySelector(".toggle");
 const p_tag = document.querySelectorAll("p.copyright.text-muted");
 const p1_tag = document.querySelectorAll("p.theme-by.text-muted");
-
 const allElements = document.querySelectorAll('*');
-console.log(allElements);
-const filteredElements = Array.from(allElements).filter(element => !element.classList.contains('container-md') && !element.classList.contains('row') && !element.classList.contains('col-xl-8') && !element.classList.contains('avatar-container') && element.tagName !== 'H1' && !element.classList.contains('post-heading') && !element.classList.contains('post-subheading') && !element.classList.contains('post-meta'));
+const filteredElements = Array.from(allElements).filter(element => !element.classList.contains('container-md') && !element.classList.contains('row') && !element.classList.contains('col-xl-8') && !element.classList.contains('avatar-container') && element.tagName !== 'H1' && !element.classList.contains('post-heading') && !element.classList.contains('post-subheading') && !element.classList.contains('fa-sun') && !element.classList.contains('fa-moon') && !element.classList.contains('post-meta'));
 
 var index = 0;
 toggle.addEventListener("click", () => {
