305085bc54f114fc0b6e2d88ba55b5ccc02e0796
nguyencongthang2509 <congthang25092003@gmail.com>
2023-02-28 20:55:17 +0700

    no message

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,6 @@ const page_link = document.querySelectorAll(".page-link");
 const toggle = document.querySelector(".toggle");
 const p_tag = document.querySelectorAll("p.copyright.text-muted");
 const p1_tag = document.querySelectorAll("p.theme-by.text-muted");
-const moon = document.querySelector("fa-mooon")
 const allElements = document.querySelectorAll('*');
 const filteredElements = Array.from(allElements).filter(element => !element.classList.contains('container-md') && !element.classList.contains('row') && !element.classList.contains('col-xl-8') && !element.classList.contains('avatar-container') && element.tagName !== 'H1' && !element.classList.contains('post-heading') && !element.classList.contains('post-subheading') && !element.classList.contains('fa-sun') && !element.classList.contains('fa-moon') && !element.classList.contains('post-meta'));
 
@@ -13,6 +12,8 @@ toggle.addEventListener("click", () => {
     for (var i = 0; i < p_tag.length; i++) {
       p_tag[i].style.color = "white !important";
       p1_tag[i].style.color = "white !important";
+      const moon = document.querySelector("fa-mooon")
+      moon.classList.toggle("dark-skin");
     }
   } else {
     toggle.firstElementChild.className = "fa fa-sun";
@@ -21,7 +22,6 @@ toggle.addEventListener("click", () => {
       p1_tag[i].style.color = "gray !important";
     }
   }
-  moon.classList.toggle("dark-skin");
   for (var i = 0; i < filteredElements.length; i++) {
     filteredElements[i].classList.toggle("dark");
   }
