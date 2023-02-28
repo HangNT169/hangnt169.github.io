f24ee69466b71a926807b2ab89a05cb66295cd9c
nguyencongthang2509 <congthang25092003@gmail.com>
2023-02-28 22:25:26 +0700

    no message

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@ const toggle = document.querySelector(".toggle");
 const p_tag = document.querySelectorAll("p.copyright.text-muted");
 const p1_tag = document.querySelectorAll("p.theme-by.text-muted");
 const allElements = document.querySelectorAll("*");
+const fasFaCircle = document.querySelectorAll(".fas.fa-circle.fa-stack-2x");
 
 const filteredElements = Array.from(allElements).filter(
   (element) =>
@@ -34,6 +35,10 @@ function changeSkin() {
       localStorage.setItem("check_skin", true);
       p_tag[i].style.color = "white !important";
       p1_tag[i].style.color = "white !important";
+    }
+    for (let i = 0; i < fasFaCircle.length; i++) {
+      fasFaCircle[i].style.color = "black !important";
+      fasFaCircle[i].style.backgroundColor = "white !important";
     }
   } else {
     toggle.firstElementChild.className = "fa fa-sun";
