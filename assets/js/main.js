536b3e1b830ea082aa592e0d91ed643c16ea8930
nguyencongthang2509 <congthang25092003@gmail.com>
2023-03-02 18:55:04 +0700

    no message

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,6 +17,7 @@ const filteredElements = Array.from(allElements).filter(
     !element.classList.contains("container-md") &&
     !element.classList.contains("row") &&
     !element.classList.contains("col-xl-8") &&
+    !element.classList.contains("nav-search") &&
     !element.classList.contains("avatar-container") &&
     element.tagName !== "H1" &&
     !element.classList.contains("post-heading") &&
